1ec5c2f63efe80dfe1b3f80c993edf4e59978016
Natasha <natasha.pearl08@gmail.com>
2021-10-11 16:16:29 +0100

    never

--- src/main/java/com/sparta/spartasimulatoreng95/definitely_no.java
+++ src/main/java/com/sparta/spartasimulatoreng95/definitely_no.java
@@ -3,7 +3,7 @@ package com.sparta.spartasimulatoreng95;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class definitely_no {
     @FXML
     private Label welcomeText;
 
