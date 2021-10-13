f55de1170adc32abe782a48e85783af2fc46d9e7
Natasha <natasha.pearl08@gmail.com>
2021-10-13 13:29:46 +0100

    implemented junit tests for TrainingHub

---
+++ src/test/java/com/sparta/simulation/model/TrainingHubShould.java
@@ -0,0 +1,59 @@
+package com.sparta.simulation.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainingHubShould {
+
+    @Test
+    public void isClosableCheck_SmallYoung_ReturnFalse() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for(int i = 0; i < 24; i++) {
+            trainees.add(new Trainee(i, Simulation.Courses.JAVA));
+        }
+        TrainingHub hub = new TrainingHub();
+        hub.setCurrentTrainees(trainees);
+        hub.setAgeInMonths(2);
+        assertEquals(false, hub.isCloseable());
+    }
+
+    @Test
+    public void isClosableCheck_SmallOld_ReturnTrue() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for(int i = 0; i < 24; i++) {
+            trainees.add(new Trainee(i, Simulation.Courses.JAVA));
+        }
+        TrainingHub hub = new TrainingHub();
+        hub.setCurrentTrainees(trainees);
+        hub.setAgeInMonths(2);
+        assertEquals(true, hub.isCloseable());
+    }
+
+    @Test
+    public void isClosableCheck_BigYoung_ReturnFalse() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for(int i = 0; i < 25; i++) {
+            trainees.add(new Trainee(i, Simulation.Courses.JAVA));
+        }
+        TrainingHub hub = new TrainingHub();
+        hub.setCurrentTrainees(trainees);
+        hub.setAgeInMonths(3);
+        assertEquals(false, hub.isCloseable());
+    }
+
+    @Test
+    public void isClosableCheck_BigOld_ReturnFalse() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for(int i = 0; i < 25; i++) {
+            trainees.add(new Trainee(i, Simulation.Courses.JAVA));
+        }
+        TrainingHub hub = new TrainingHub();
+        hub.setCurrentTrainees(trainees);
+        hub.setAgeInMonths(3);
+        assertEquals(false, hub.isCloseable());
+    }
+}
