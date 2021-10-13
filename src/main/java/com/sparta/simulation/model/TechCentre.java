3740c6cfe0cbd448494b8e3be868f1bfb5187866
TobiOl <1712264@brunel.ac.uk>
2021-10-13 14:33:26 +0100

    Implementation done, working on unit tests

--- src/main/java/com/sparta/simulation/model/TechCentre.java
+++ src/main/java/com/sparta/simulation/model/TechCentre.java
@@ -3,20 +3,37 @@ package com.sparta.simulation.model;
 import java.util.ArrayList;
 
 public class TechCentre extends Centre{
-    int CAPACITY;
+    int CAPACITY = 200;
     int id;
     private final String centreType = "TechCentre";
-    final int TotalCapacity = 200;
-    ArrayList<Simulation.Courses> centreCourseType = new ArrayList<>();
-    ArrayList<Trainee> currentTrainees;
-    ArrayList<Trainee> returnToWaitingList;
+    private String centreCourseType;
+    private ArrayList<Trainee> currentTrainees = new ArrayList<>();
+    private int gracePeriod = 0;
+    ArrayList<Trainee> returnToWaitingList = new ArrayList<>();
 
-    public void setCentreCourseType(ArrayList<Simulation.Courses> centreCourseType) {
+    public String getCentreCourseType() {
+        return centreCourseType;
+    }
+
+    public void setCentreCourseType(String centreCourseType) {
         this.centreCourseType = centreCourseType;
     }
 
-    public ArrayList<Simulation.Courses> getCentreCourseType() {
-        return centreCourseType;
+    @Override
+    public ArrayList<Trainee> getCurrentTrainees() {
+        return currentTrainees;
+    }
+
+    public void setCurrentTrainees(ArrayList<Trainee> currentTrainees) {
+        this.currentTrainees = currentTrainees;
+    }
+
+    public ArrayList<Trainee> getReturnToWaitingList() {
+        return returnToWaitingList;
+    }
+
+    public void setReturnToWaitingList(ArrayList<Trainee> returnToWaitingList) {
+        this.returnToWaitingList = returnToWaitingList;
     }
 
     public int getCAPACITY() {
@@ -35,19 +52,37 @@ public class TechCentre extends Centre{
         this.id = id;
     }
 
+    public int getGracePeriod() {
+        return gracePeriod;
+    }
+
+    public void setGracePeriod(int gracePeriod) {
+        this.gracePeriod = gracePeriod;
+    }
+
     // Concrete method
     void addTrainees(ArrayList<Trainee> incomingTrainees) {
-        for (int i = 0; i < incomingTrainees.size(); i++) {
-            if (incomingTrainees.get(i).getTraineeCourse().equals(getCentreCourseType())){
-
+        if (incomingTrainees.size() == 0) { return; }
+        int capacityDiff = CAPACITY - (getCurrentTrainees().size());
+        if (capacityDiff >= incomingTrainees.size()) {
+            for (Trainee incomingTrainee : incomingTrainees) {
+                if (incomingTrainee.getTraineeCourse().name().equals(getCentreCourseType())) {
+                    currentTrainees.add(incomingTrainee);
+                } else {
+                    returnToWaitingList.add(incomingTrainee);
+                }
             }
-            else{
-                returnToWaitingList.add(incomingTrainees.get(i));
-            }
+        } else {
+            ArrayList<Trainee> joining = (ArrayList<Trainee>) incomingTrainees.subList(0, capacityDiff);
+            ArrayList<Trainee> remainder = (ArrayList<Trainee>) incomingTrainees.subList(capacityDiff, incomingTrainees.size());
+            currentTrainees.addAll(joining);
+            returnToWaitingList.addAll(remainder);
         }
-        // Try and add as many as randomGen dictates up to CAPACITY.
-        // If it cannot add all in randomGen it will add them to returnToWaitingList
-    }
+
+        }
+
+
+
     ArrayList<Trainee> closeCentre() {
         return currentTrainees;
     }
@@ -56,7 +91,8 @@ public class TechCentre extends Centre{
     @Override
     boolean isCloseable() {
         boolean result = false;
-        if (getCAPACITY() < 25){
+        setGracePeriod(getGracePeriod()+1);
+        if (getCurrentTrainees().size() < 25 && getGracePeriod() > 2){
             result = true;
         }
         return result;
