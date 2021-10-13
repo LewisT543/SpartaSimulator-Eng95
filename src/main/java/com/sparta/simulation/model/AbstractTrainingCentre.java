5446ab6d48e6a020c24e6621080cf6ab14b5733b
Lewis <lewisthornton@btinternet.com>
2021-10-13 11:34:04 +0100

    Initial boilerplate for abstract TrainingCentre class

--- src/main/java/com/sparta/simulation/model/AbstractTrainingCentre.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.simulation.model;
-
-public abstract class AbstractTrainingCentre {
-    private int CAPACITY;
-    private int id;
-    private ArrayList<Trainee> currentTrainees;
-    private ArrayList<Trainee> returnToWaitingList;
-}
