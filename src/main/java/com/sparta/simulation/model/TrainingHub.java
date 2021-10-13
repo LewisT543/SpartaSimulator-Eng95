6c4688cd90588374669d9942c9569761cb27f90a
Natasha <natasha.pearl08@gmail.com>
2021-10-13 13:29:46 +0100

    implemented junit tests for TrainingHub

--- src/main/java/com/sparta/simulation/model/TrainingHub.java
+++ src/main/java/com/sparta/simulation/model/TrainingHub.java
@@ -1,6 +1,19 @@
 package com.sparta.simulation.model;
 
+import java.util.ArrayList;
+
 public class TrainingHub extends Centre {
+
+    // TODO: delete this once it's been added to Centre class
+    public void setCurrentTrainees(ArrayList<Trainee> trainees) {
+        this.currentTrainees = trainees;
+    }
+
+    // TODO: delete this once it's been added to Centre class
+    public void setAgeInMonths(int age) {
+        this.ageInMonths = age;
+    }
+
     @Override
     boolean isCloseable() {
         if (currentTrainees.size() < 25 && ageInMonths > 2) {
