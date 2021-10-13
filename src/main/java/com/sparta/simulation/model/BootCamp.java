6a9f96f47ce802048fcb90310edec045e9787369
TeamPapaya <34426841+edmundackah@users.noreply.github.com>
2021-10-13 15:15:32 +0100

    bootcamp implemented with tests

--- src/main/java/com/sparta/simulation/model/BootCamp.java
+++ src/main/java/com/sparta/simulation/model/BootCamp.java
@@ -3,19 +3,25 @@ package com.sparta.simulation.model;
 import java.util.ArrayList;
 
 public class BootCamp extends Centre {
-    int lifespan;
-    boolean isGracePeriod = false;
+    private boolean isGracePeriod = false;
+    private int consecutiveMonthsBelow;
 
-    public BootCamp(int id, int capacity) {
+
+    public BootCamp(int id) {
         this.setId(id);
-        this.setCAPACITY(capacity);
-        lifespan = 0;
+        this.setCAPACITY(500);
+        setAgeInMonths(0);
+        consecutiveMonthsBelow = 0;
         setCurrentTrainees(new ArrayList<>());
         setReturnToWaitingList(new ArrayList<>());
     }
 
     @Override
     public void addTrainees(ArrayList<Trainee> incomingTrainees) {
+        if (incomingTrainees == null) {
+            throw new IllegalArgumentException("ArrayList cannot be null");
+        }
+
         // add trainees (when we can)
         // excess added to separate ArrayList
 
@@ -28,13 +34,22 @@ public class BootCamp extends Centre {
             }
         });
 
-        lifespan++;
-        isGracePeriod = lifespan <= 2;
+        setAgeInMonths(getAgeInMonths()+1);
+
+        isGracePeriod = getAgeInMonths() <= 2;
         // keep track of months - determine grace period, and the 3 month thing
+        // separate counter for the 3 month thing
+        // <=25 trainees, increment counter
+        // > 25 trainees, reset counter to 0
+        if (getCurrentTrainees().size() <= 25) {
+            consecutiveMonthsBelow++;
+        } else {
+            consecutiveMonthsBelow = 0;
+        }
     }
 
     @Override
     public boolean isCloseable() {
-        return getCurrentTrainees().size() <= 25 && !isGracePeriod;
+        return getCurrentTrainees().size() <= 25 && !isGracePeriod && consecutiveMonthsBelow >= 3;
     }
 }
--- src/test/java/com/sparta/simulation/model/BootCampShould.java
+++ src/test/java/com/sparta/simulation/model/BootCampShould.java
@@ -13,7 +13,7 @@ public class BootCampShould {
     private BootCamp bootCamp;
     @BeforeEach
     public void setUp() {
-        bootCamp = new BootCamp(1, 500);
+        bootCamp = new BootCamp(1);
     }
 
     // testing isCloseable()
@@ -53,36 +53,143 @@ public class BootCampShould {
 
     @Test
     public void isCloseable_TwoMonthGracePeriod_MonthOne_Exactly25Trainees_ReturnFalse() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.setCurrentTrainees(trainees);
+
         for (int i = 0; i < 1; i ++) {
             // add some trainees
-            bootCamp.addTrainees(new ArrayList<>(List.of(
-                    new Trainee(1),
-                    new Trainee(2))));
-            /*
-            bootCamp.addTrainees(IntStream.rangeClosed(0, 24).boxed().map((x) -> {
-                new Trainee(x);
-            }).toList());
-
-             */
+            bootCamp.addTrainees(new ArrayList<>());
         }
         assertFalse(bootCamp.isCloseable());
     }
 
-//    @Test
-//    public void isCloseable_TwoMonthGracePeriod_MonthThree_Exactly25Trainees_ReturnTrue() {
-//        ArrayList<Trainee> trainees = new ArrayList<>();
-//        for (int i = 0; i < 25; i++) {
-//            trainees.add(new Trainee(i));
-//        }
-//        bootCamp.setCurrentTrainees(trainees);
-//
-//        for (int i = 0; i < 3; i ++) {
-//            // add some trainees
-//            bootCamp.addTrainees(new ArrayList<>());
-//        }
-//        assertTrue(bootCamp.isCloseable());
-//    }
+    @Test
+    public void isCloseable_TwoMonthGracePeriod_MonthThree_Exactly25Trainees_ReturnTrue() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.setCurrentTrainees(trainees);
 
+        for (int i = 0; i < 3; i ++) {
+            // add some trainees
+            bootCamp.addTrainees(new ArrayList<>());
+        }
+        assertTrue(bootCamp.isCloseable());
+    }
+
+    @Test
+    public void isCloseable_TwoMonthsFewer25Trainees_ThirdMonthMoreThan25Trainees_ReturnFalse() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.setCurrentTrainees(trainees);
+
+        // simulate 2 months
+        for (int i = 0; i < 2; i++) {
+            bootCamp.addTrainees(new ArrayList<>());
+        }
+        for (int i = 0; i < 50; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.setCurrentTrainees(trainees);
+
+        // simulate 1 month
+        bootCamp.addTrainees(new ArrayList<>());
+
+        assertFalse(bootCamp.isCloseable());
+
+    }
+
+    // testing addTrainees
+    // test both currentTrainees and returnToWaitingList
+    // 1. 0> and <500 trainees
+    // 2. exactly 500 trainees
+    // 3. more than 500 trainees
+    // 4. 0 trainees
+
+    @Test
+    public void addTrainees_WhenTraineesAreGreaterThanZero_LessThan500_ShouldBeInCurrentTrainees() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.addTrainees(trainees);
+        assertEquals(10, bootCamp.getCurrentTrainees().size());
+    }
+
+    @Test
+    public void addTrainees_WhenTraineesAreGreaterThanZero_LessThan500_ReturnToWaitingListSizeIsZero() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.addTrainees(trainees);
+        assertEquals(0, bootCamp.getReturnToWaitingList().size());
+    }
+
+    @Test
+    public void addTrainees_WhenTraineesIs500_ShouldBeInCurrentTrainees() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int x = 0; x < 500; x++) {
+            trainees.add(new Trainee(x));
+        }
+        bootCamp.addTrainees(trainees);
+        assertEquals(500, bootCamp.getCurrentTrainees().size());
+    }
+
+    @Test
+    public void addTrainees_WhenTraineesIs500_ReturnToWaitingListSizeIsZero() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int i = 0; i < 500; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.addTrainees(trainees);
+        assertEquals(0, bootCamp.getReturnToWaitingList().size());
+    }
+
+    @Test
+    public void addTrainees_WhenTraineesAreMoreThan500_500ShouldBeInCurrentTrainees() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int i = 0; i < 510; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.addTrainees(trainees);
+        assertEquals(500, bootCamp.getCurrentTrainees().size());
+    }
+
+    @Test
+    public void addTrainees_WhenTraineesAreMoreThan500_ExcessShouldBeInReturnToWaitingList() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        for (int i = 0; i < 510; i++) {
+            trainees.add(new Trainee(i));
+        }
+        bootCamp.addTrainees(trainees);
+        assertEquals(10, bootCamp.getReturnToWaitingList().size());
+    }
+
+    @Test
+    public void addTrainees_WhenTraineesIsZero_ZeroInCurrentTrainees() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        bootCamp.addTrainees(trainees);
+        assertEquals(0, bootCamp.getCurrentTrainees().size());
+    }
+
+    @Test
+    public void addTrainees_WhenTraineesIsZero_ZeroInWaitingList() {
+        ArrayList<Trainee> trainees = new ArrayList<>();
+        bootCamp.addTrainees(trainees);
+        assertEquals(0, bootCamp.getReturnToWaitingList().size());
+    }
+
+    @Test
+    public void addTrainees_WhenTraineesIsNull_ThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> bootCamp.addTrainees(null));
+    }
 
     // 1. 2 month grace period, can't close no matter what
     // 1.1 if < 25, don't close
