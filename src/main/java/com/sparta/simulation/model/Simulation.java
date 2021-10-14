b2704372b194f51ac6c96a6d0750dac43b9b3e41
Dwhite77 <dan_white18@live.com>
2021-10-14 17:12:19 +0100

    made the logic work in simulation, remade the view, a few small edits within centre, added getters for age in bootcamp so that we can get their age in months

--- src/main/java/com/sparta/simulation/model/Simulation.java
+++ src/main/java/com/sparta/simulation/model/Simulation.java
@@ -17,10 +17,10 @@ public class Simulation {
     private int totalTrainingCentres =0;
     private int traineeID = 0;
     private int bootcampCount =0;
-    private int trainingHubCount = 0;
-    private int closedBootcampCount =0;
-    private int closedTrainingHubCount =0;
-    private int closedTechCentreCount =0;
+//    private int trainingHubCount = 0;
+//    private int closedBootcampCount =0;
+//    private int closedTrainingHubCount =0;
+//    private int closedTechCentreCount =0;
 
     public enum Courses{DEVOPS,JAVA,DATA,CSHARP,BUSINESS} // is this allowed to be public?
 
@@ -116,7 +116,7 @@ public class Simulation {
     // Currently not working
     public void distributeTraineesToCentres(Long seed) {
         for(Centre centre: trainingCentres) {
-            int trainingIntake = UtilityMethods.generateRandomInt(0, 51, null);
+            int trainingIntake = UtilityMethods.generateRandomInt(0, 51, seed);
             while (centre.getCAPACITY() > centre.getCurrentTrainees().size() && reallocatedTrainees.size() > 0
                     && trainingIntake > 0) {
                 if (centre instanceof TechCentre) {
@@ -124,6 +124,7 @@ public class Simulation {
                         centre.addTrainee(reallocatedTrainees.getFirst());
                         reallocatedTrainees.pop();
                         trainingIntake--;
+                        break;
                     }
                 }
 
@@ -131,6 +132,7 @@ public class Simulation {
                     centre.addTrainee(reallocatedTrainees.getFirst());
                     reallocatedTrainees.pop();
                     trainingIntake--;
+                    break;
                 }
             }
             while (centre.getCAPACITY() > centre.getCurrentTrainees().size() && newTrainees.size() > 0
@@ -140,12 +142,14 @@ public class Simulation {
                         centre.addTrainee(newTrainees.getFirst());
                         newTrainees.pop();
                         trainingIntake--;
+                        break;
                     }
                 }
                 else {
                     centre.addTrainee(newTrainees.getFirst());
                     newTrainees.pop();
                     trainingIntake--;
+                    break;
                 }
             }
             centre.setAgeInMonths(centre.getAgeInMonths() + 1);
@@ -174,6 +178,7 @@ public class Simulation {
 
     public void reallocateTrainees(int i){
         reallocatedTrainees.addAll(trainingCentres.get(i).getCurrentTrainees());
+        trainingCentres.get(i).setCurrentTrainees(new ArrayList<>());
     }
 
     public void closeCentre(int i){
