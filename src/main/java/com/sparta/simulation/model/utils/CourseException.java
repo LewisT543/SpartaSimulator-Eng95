da9b8a099022b026a9353668d9c363e43c7fa26a
karina-talibzhanova <karina.talibzhanova@gmail.com>
2021-10-14 15:04:30 +0100

    created new runtime exception in the case that the trainee has a course that hasn't been handled

---
+++ src/main/java/com/sparta/simulation/model/utils/CourseException.java
@@ -0,0 +1,7 @@
+package com.sparta.simulation.model.utils;
+
+public class CourseException extends RuntimeException {
+    public CourseException(String message) {
+        super(message);
+    }
+}
